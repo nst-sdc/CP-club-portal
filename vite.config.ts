2a93886bebbfd557e45cabbfb264e93c77a8bb6e
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2024-11-23 02:34:37 +0530

    use vite-plugin-vercel to create api endpoints

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel'
 
 // vivek W
 // follow us on nstsdc.org
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vercel(), react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
